865668e50ea7a71e8c600405e9b7e13974348bec
chanwoo <chanwoo0607@naver.com>
2024-10-05 19:27:10 +0900

    test : 커밋 테스트

--- src/pages/community/Community.js
+++ src/pages/community/Community.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import CommunityButton from "../../components/CommunityButton";
+
 export default function Community() {
     return (
         <div>
-            <CommunityButton></CommunityButton>
             커뮤니티 페이지
         </div>
     )
