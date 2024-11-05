d9cfb0a6790562adcebf8716f2c23b1580636985
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-11-05 15:08:44 +0900

    refactor: 회원가입 시 선택 필드 더미데이터로 분리

--- src/pages/login/SignUp.js
+++ src/pages/login/SignUp.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommonHeader from "../../components/CommonHeader";
-import {DOMAIN_NAME} from "../../App";
+import { DOMAIN_NAME } from "../../App";
+import { leagueFields, teamInfoOptions, soccerTeamDivisionOptions, basketballTeamDivisionOptions } from "../../data/login";
 
 export default function SignUp() {
-    const [email] = useState('example@kookim.ac.kr');  // 구글 로그인에서 가져오는 이메일
-    const [name, setName] = useState('승부각');  // 구글 로그인에서 가져오는 이름, 수정 가능
+    const [email, setEmail] = useState('');  // 구글 로그인에서 가져오는 이메일
+    const [name, setName] = useState('');  // 구글 로그인에서 가져오는 이름, 수정 가능
     const [isTeamLeader, setIsTeamLeader] = useState(false);
     const [leagueField, setLeagueField] = useState(''); // 북악리그 분야
     const [teamInfo, setTeamInfo] = useState(''); // 소속 정보
@@ -14,62 +15,13 @@ export default function SignUp() {
     const [isPrivacyPolicyAccepted, setIsPrivacyPolicyAccepted] = useState(false);
     const [isAllAccepted, setIsAllAccepted] = useState(false); // 전체 동의 체크박스 상태
 
-    // 북악리그 분야 리스트
-    const leagueFields = ['축구', '농구'];
-
-    // 소속 정보 리스트 (축구와 농구 선택 시 다르게 나타남)
-    const teamInfoOptions = {
-        축구: ["건축대학", "경상대학", "경영대학", "경영대학 AI빅데이터융합경영학과", "경영대학 경영정보학부", "경영대학 재무금융회계학부", "과학기술대학 나노전자물리학과",
-            "과학기술대학 산림환경시스템학과 & 임산생명공학과", "글로벌인문지역대학 & 사회과학대학", "법과대학", "사회과학대학 미디어광고학부",
-            "사회과학대학 사회학과", "사회과학대학 정치외교학과", "사회과학대학 행정학과", "소프트웨어융합대학", "자동차융합대학", "창의공과대학 건설시스템공학부",
-            "창의공과대학 기계공학부", "창의공과대학 신소재공학부", "창의공과대학 전자공학부", "체육대학", "학군단", "기타"],
-        농구: ["건축대학", "경상대학", "경영대학", "과학기술대학", "글로벌인문지역대학 & 사회과학대학", "법과대학", "사회과학대학 미디어광고학부",
-            "소프트웨어융합대학", "자동차융합대학", "창의공과대학 건설시스템공학부", "창의공과대학 전자공학부", "기타"]
-
-    };
-
-    // 축구 팀명 리스트
-    const soccerTeamDivisionOptions = {
-        "건축대학": ['FC SCALE'],
-        "경상대학": ['쉐도우'],
-        "경영대학": ['바이퍼스'],
-        "경영대학 AI빅데이터융합경영학과": ['BAS'],
-        "경영대학 경영정보학부": ['FC BIT'],
-        "경영대학 재무금융회계학부": ['FIAV'],
-        "과학기술대학 나노전자물리학과": ['네피스트'],
-        "과학기술대학 산림환경시스템학과 & 임산생명공학과": ['포레스트'],
-        "글로벌인문지역대학 & 사회과학대학": ['야스크'],
-        "법과대학": ['컬스'],
-        "사회과학대학 미디어광고학부": ['로니즈'],
-        "사회과학대학 사회학과": ['아레스'],
-        "사회과학대학 정치외교학과": ['꺄멜레온'],
-        "사회과학대학 행정학과": ['판타지스타'],
-        "소프트웨어융합대학": ['CS슈팅'],
-        "자동차융합대학": ['KAFA'],
-        "창의공과대학 건설시스템공학부": ['레알모멘트'],
-        "창의공과대학 기계공학부": ['공사랑'],
-        "창의공과대학 신소재공학부": ['포리원'],
-        "창의공과대학 전자공학부": ['KESA'],
-        "체육대학": ['한마음'],
-        "학군단": ['아이구'],
-        "기타": ['FOCUS', 'NS'],
-    };
-
-    // 농구 팀명 리스트
-    const basketballTeamDivisionOptions = {
-        "건축대학": ['아키템포'],
-        "경상대학": ['맨큐'],
-        "경영대학": ['CEO'],
-        "과학기술대학": ['STEP'],
-        "글로벌인문지역대학 & 사회과학대학": ['FM'],
-        "법과대학": ['MBL'],
-        "사회과학대학 미디어광고학부": ['워닝'],
-        "소프트웨어융합대학": ['CS점핑'],
-        "자동차융합대학": ['FWD'],
-        "창의공과대학 건설시스템공학부": ['GG'],
-        "창의공과대학 전자공학부": ['코티스트'],
-        "기타": ['KUBA', 'TAB'],
-    };
+    // localStorage에서 이메일과 이름 가져오기
+    useEffect(() => {
+        const storedEmail = localStorage.getItem('userEmail');
+        const storedName = localStorage.getItem('userName');
+        if (storedEmail) setEmail(storedEmail);
+        if (storedName) setName(storedName);
+    }, []);
 
     // 전체 동의 체크박스 처리
     const handleAllAcceptedChange = (e) => {
@@ -94,14 +46,9 @@ export default function SignUp() {
             if (res.data.success) {
                 console.log('회원가입 성공');
 
-                // 서버에서 받은 Authorization 헤더에서 토큰 추출
                 const token = res.headers['authorization'];
-
-                // Authorization 헤더 형식이 'Bearer <token>'일 경우 토큰만 추출
-                const bearerToken = token && token.split(' ')[1]; // 'Bearer ' 다음의 실제 토큰 부분만 가져옴
-
+                const bearerToken = token && token.split(' ')[1];
                 if (bearerToken) {
-                    // 로그인 페이지로 토큰을 넘겨 이동 처리
                     window.location.href = `/signin?token=${bearerToken}`;
                 } else {
                     console.log('토큰 추출 실패');
@@ -118,7 +65,7 @@ export default function SignUp() {
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-800">
-            <CommonHeader/>
+            <CommonHeader />
 
             <div className="flex flex-1 justify-center items-center">
                 <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
