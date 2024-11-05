7e6926330ef5adf454edf876e6e650ab2eebc15f
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-11-05 15:11:03 +0900

    feat: 로그인 시 유저 이메일, 이름 정보 가져올 수 있도록

--- src/pages/login/SignIn.js
+++ src/pages/login/SignIn.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import CommonHeader from "../../components/CommonHeader";
+import { jwtDecode } from 'jwt-decode';
 import { DOMAIN_NAME } from "../../App";
 
 export default function SignIn() {
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleLoginSuccess = (response) => {
-        console.log(response); // 토큰 또는 기타 로그인 성공 정보를 출력
+        const credential = response.credential;
+
+        // JWT 토큰을 디코딩하여 사용자 정보 추출
+        const userInfo = jwtDecode(credential);
+
+        // 이메일과 이름을 localStorage에 저장하여 회원가입 시 활용
+        localStorage.setItem('userEmail', userInfo.email);
+        localStorage.setItem('userName', userInfo.name);
 
         // 서버로 토큰 전송 후, 서버에서 리다이렉트 처리
         window.location.href = `${DOMAIN_NAME}/oauth2/authorization/google`;
@@ -23,7 +31,7 @@ export default function SignIn() {
                     <h1 className="text-2xl text-white mb-4">반가워요!</h1>
                     <p className="text-2xl text-white mb-4">
                         국민대학교 북악리그 전용 웹사이트 <br/>
-                        <strong>&lt;우리 오늘, 승부 각 : 勝 북악&gt;</strong> 입니다.
+                        <strong className="!text-white">&lt;우리 오늘, 승부 각 : 勝 북악&gt;</strong> 입니다.
                     </p>
 
                     {/* 비디오 가운데 정렬 및 크기 조절 */}
