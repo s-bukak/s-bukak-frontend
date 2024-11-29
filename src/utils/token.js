8ecf97c0c1e84d7a83b0c140b179986dc02d7b9a
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2024-11-29 19:06:43 +0900

    feat: 토큰 관리 파일 from 규원 체리픽 🍒

---
+++ src/utils/token.js
@@ -0,0 +1,21 @@
+import { jwtDecode } from "jwt-decode";
+
+export const getToken = () => localStorage.getItem("access_token");
+
+export const setToken = (token) => localStorage.setItem("access_token", token);
+
+export const removeToken = () => localStorage.removeItem("access_token");
+
+export const isTokenValid = () => {
+    const token = getToken();
+
+    if (!token) return false;
+
+    try {
+        const decoded = jwtDecode(token);
+        return decoded.exp * 1000 > Date.now(); // 만료 시간 확인
+    } catch (error) {
+        console.error("JWT 디코딩 오류:", error);
+        return false;
+    }
+};
