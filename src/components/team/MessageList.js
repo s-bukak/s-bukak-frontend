7a44200fa47b6a64ab84e0b9b13d6dd5edce0580
suwith <suwith@kookmin.ac.kr>
2024-12-02 23:51:07 +0900

    design: 응원메세지 컴포넌트 피그마 디자인 반영

--- src/components/team/MessageList.js
+++ src/components/team/MessageList.js
@@ -1,8 +1,8 @@
 import {useEffect, useRef, useState} from "react";
 import {cleanbotMessage, homePlaceHolder} from "../../utils/MessageUtils";
 import anonymous from "../../assets/images/anonymous.svg";
-import {IoIosSend} from "react-icons/io";
-import {FaRegTrashAlt} from "react-icons/fa";
+import sendIcon from "../../assets/icons/send.svg";
+import userImage from "../../assets/images/userImage.jpg"
 import useTeamMsg from "../../hooks/useTeamMsg";
 import {useRecoilState} from "recoil";
 import {teamIdState} from "../../state/sportTabState";
@@ -13,6 +13,7 @@ import {DOMAIN_NAME} from "../../App";
 import useDeleteMsg from "../../hooks/useDeleteMsg";
 import {decodeUserInfo} from "../../utils/UserUtils";
 import {getToken, isTokenValid} from "../../utils/token";
+import {FaRegTrashAlt} from "react-icons/fa";
 
 const MessageList = ({style}) => {
     const [input, setInput] = useState("");
@@ -193,9 +194,9 @@ const MessageList = ({style}) => {
     return (
         <div className="flex flex-col items-center w-full h-full mt-2">
             <div
-                className="flex flex-col justify-between w-full h-full bg-white rounded-2xl border border-gray-400 p-4">
+                className="flex flex-col justify-between w-full h-full bg-white rounded-2xl border border-gray-400 p-5">
                 <div
-                    className="flex flex-col w-full overflow-y-auto flex-1"
+                    className="flex flex-col w-full overflow-y-auto flex-1 scrollbar-hide"
                     style={{...style}}
                 >
                     <div
@@ -206,147 +207,113 @@ const MessageList = ({style}) => {
                         }`}
                     >
                         {chatList.length > 0 ? (
-                            chatList.map((comment) => (
-                                <div key={comment.id} className="w-full">
-                                    <div
-                                        className={`flex ${
-                                            comment.userId === user?.userId
-                                                ? "justify-end"
-                                                : "justify-start"
-                                        } mb-1`}
-                                    >
-                                        {comment.userId === user?.userId ? (
-                                            <>
-                                                <div className="flex flex-col items-end max-w-xs">
-                                                    <div className="flex items-center mb-1">
-                            <span className="text-sm font-semibold">
-                              {user?.userName}
-                            </span>
-                                                    </div>
-                                                    <div
-                                                        className="bg-blue-100 text-gray-700 p-2.5 rounded-lg text-sm break-words">
-                                                        {comment.content}
-                                                    </div>
-                                                    <span className="text-xs text-gray-500 mt-1">
-                            {comment.createdAt}
-                          </span>
-                                                </div>
-                                                <div
-                                                    className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 ml-3">
-                                                    <img src={comment.userImage} alt="user"/>
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <div
-                                                    className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 mr-3">
-                                                    <img
-                                                        src={
-                                                            comment.isAnonymous
-                                                                ? anonymous
-                                                                : comment.userImage
-                                                        }
-                                                        alt="user"
-                                                    />
-                                                </div>
-                                                <div className="flex flex-col items-start max-w-xs">
-                                                    <div className="flex items-center mb-1">
-                            <span className="text-sm font-semibold">
-                              {comment.isAnonymous ? "익명" : comment.username}
-                            </span>
-                                                    </div>
-                                                    <div className="flex flex-row">
-                                                        <div
-                                                            className="bg-gray-100 text-gray-700 p-2.5 rounded-lg text-sm break-words">
-                                                            {comment.content}
-                                                        </div>
-                                                        {teamInfo?.canUpdatePlayers &&
-                                                            !comment.isHidden && (
-                                                                <div className="p-1.5">
-                                                                    <FaRegTrashAlt
-                                                                        className=" flex justify-center  h-full w-3 text-red-500"
-                                                                        onClick={() => handleDelete(comment.id)}
-                                                                    />
-                                                                </div>
-                                                            )}
-                                                    </div>
+                            chatList.map((comment) => {
+                                const isCurrentUser = comment.userId === user?.userId;
+                                const alignment = isCurrentUser ? "justify-end" : "justify-start";
+                                const rowDirection = isCurrentUser ? "flex-row-reverse" : "flex-row";
+                                const imageMargin = isCurrentUser ? "ml-2" : "mr-2";
 
-                                                    <span className="text-xs text-gray-500 mt-1">
-                            {comment.createdAt}
-                          </span>
+                                // Check if the comment is anonymous and use the anonymous image
+                                const displayImage = comment.isAnonymous ? anonymous : comment.userImage;
+                                const messageUsername = comment.isAnonymous ? "익명" : comment.username;
+                                const messageContent = comment.isHidden ? cleanbotMessage.content : comment.content;
+                                const userImage = comment.isHidden ? cleanbotMessage.userImage : displayImage;
+                                const username = comment.isHidden ? cleanbotMessage.username : messageUsername;
+
+                                return (
+                                    <div key={comment.id} className="w-full">
+                                        <div className={`flex ${alignment} mb-4`}>
+                                            <div className={`flex ${rowDirection} w-full`}>
+                                                <div
+                                                    className={`w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 ${
+                                                        comment.isHidden ? "mr-2" : imageMargin
+                                                    }`}
+                                                >
+                                                    <img src={userImage} alt="user"
+                                                         className="w-full h-full object-contain"/>
                                                 </div>
-                                            </>
-                                        )}
-                                    </div>
-                                    {comment.isHidden && (
-                                        <div className="flex justify-start mb-3 w-full">
-                                            <div
-                                                className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-200 mr-3">
-                                                <img src={cleanbotMessage.userImage} alt="cleanbot"/>
-                                            </div>
-                                            <div className="flex flex-col items-start max-w-xs">
-                                                <div className="flex items-center mb-1">
-                          <span className="text-sm font-semibold">
-                            {cleanbotMessage.username}
-                          </span>
-                                                </div>
-                                                <div className="flex flex-row">
-                                                    <div
-                                                        className="bg-gray-100 text-gray-700 p-2.5 rounded-lg text-sm break-words">
-                                                        {cleanbotMessage.content}
+                                                <div
+                                                    className={`flex flex-col items-start bg-gray-100 p-2.5 px-3.5 rounded-2xl ${
+                                                        comment.isHidden ? "mr-3" : imageMargin
+                                                    } ${!comment.isHidden && !teamInfo?.canUpdatePlayers ? "flex-grow" : "w-4/5"}`}
+                                                >
+                                                    <div className="flex flex-row justify-between w-full">
+                                                        <span className="text-sm font-semibold">{username}</span>
+                                                        {!comment.isHidden && (
+                                                            <span
+                                                                className="text-xs text-gray-500 mt-1">{comment.createdAt}</span>
+                                                        )}
                                                     </div>
+                                                    <div
+                                                        className="text-gray-700 text-sm break-words">{messageContent}</div>
                                                 </div>
+                                                {!comment.isHidden && teamInfo?.canUpdatePlayers && (
+                                                    <div className="p-1.5">
+                                                        <FaRegTrashAlt
+                                                            className="h-full w-3 text-red-500 cursor-pointer"
+                                                            onClick={() => handleDelete(comment.id)}
+                                                        />
+                                                    </div>
+                                                )}
                                             </div>
                                         </div>
-                                    )}
-                                </div>
-                            ))
+                                    </div>
+                                );
+                            })
                         ) : (
                             <div className="flex justify-center items-center text-gray-400 text-center w-full h-full">
                                 메시지가 없습니다. 응원 메시지를 남겨보세요!
                             </div>
                         )}
+
                         {chatList.length > 0 && <div ref={messageEndRef}></div>}
                     </div>
                 </div>
-
-                <div className="flex items-center w-full p-1 pl-2.5 rounded-xl bg-gray-100 mt-4 border border-gray-400">
-                    <div className="flex items-center" onClick={handleInputClick}>
-                        <input
-                            id="checked-checkbox"
-                            type="checkbox"
-                            checked={isAnonymous}
-                            onChange={handleCheckboxChange}
-                            disabled={!isTokenValid()}
-                            className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-400 rounded checked:bg-gray-500 focus:ring-0 dark:bg-gray-500 dark:border-gray-500"
-                        />
-                        <label
-                            htmlFor="checked-checkbox"
-                            className="ms-2 text-sm font-medium text-gray-400"
-                        >
-                            익명
-                        </label>
+                <div className="flex flex-row w-full items-center justify-center mt-4">
+                    <div className="w-11 h-11 rounded-full overflow-hidden border-2 border-gray-200 mr-2">
+                        <img src={user?.userImage || userImage} alt={user?.userName}
+                             className="w-full h-full object-contain"/>
                     </div>
-                    <div className="ml-3 h-6 border-l-2 border-gray-300"></div>
-                    <input
-                        type="text"
-                        placeholder={
-                            isTokenValid()
-                                ? teamInfo
-                                    ? `${teamInfo.name} 선수들에게 응원 메세지를 남겨보세요!` : homePlaceHolder
-                                : "로그인이 필요한 기능입니다."
 
-                        }
-                        value={input}
-                        onChange={handleInputChange}
-                        onKeyDown={handleKeyPress}
-                        disabled={!isTokenValid()}
-                        className="flex-1 text-sm outline-none px-2 py-2 bg-gray-100"
-                    />
+                    <div
+                        className="flex flex-grow items-center  p-1 pl-2.5 rounded-xl bg-gray-100 border border-gray-400">
+                        <div className="flex items-center" onClick={handleInputClick}>
+                            <input
+                                id="checked-checkbox"
+                                type="checkbox"
+                                checked={isAnonymous}
+                                onChange={handleCheckboxChange}
+                                disabled={!isTokenValid()}
+                                className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-400 rounded checked:bg-gray-500 focus:ring-0 dark:bg-gray-500 dark:border-gray-500"
+                            />
+                            <label
+                                htmlFor="checked-checkbox"
+                                className="ms-2 text-sm font-medium text-gray-400"
+                            >
+                                익명
+                            </label>
+                        </div>
+                        <div className="ml-3 h-6 border-l-2 border-gray-300"></div>
+                        <input
+                            type="text"
+                            placeholder={
+                                isTokenValid()
+                                    ? teamInfo
+                                        ? `${teamInfo.name} 선수들에게 응원 메세지를 남겨보세요!`
+                                        : homePlaceHolder
+                                    : "로그인이 필요한 기능입니다."
+                            }
+                            value={input}
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyPress}
+                            disabled={!isTokenValid()}
+                            className="flex-1 text-sm outline-none px-2 py-2 bg-gray-100"
+                        />
 
-                    <button onClick={submitComment} disabled={!isTokenValid()}>
-                        <IoIosSend color="gray-500" className="w-8 h-8"/>
-                    </button>
+                        <button onClick={submitComment} disabled={!isTokenValid()}>
+                            <img src={sendIcon} alt={sendIcon} className="w-5 h-8 mr-2"/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/utils/UserUtils.js
+++ src/utils/UserUtils.js
@@ -1,24 +1,26 @@
-import { jwtDecode } from "jwt-decode";
-import { getToken } from "./token";
+import {jwtDecode} from "jwt-decode";
+import {getToken} from "./token";
+
+import userImage from "../assets/images/userImage.jpg";
 
 /**
  * JWT에서 사용자 정보를 디코딩하는 함수
  * @returns {{ userId: number, userName: string, userImage: string } | null} 사용자 정보 객체 또는 null
  */
 export const decodeUserInfo = () => {
-  const token = getToken();
-  if (!token) return null;
+    const token = getToken();
+    if (!token) return null;
 
-  try {
-    const decoded = jwtDecode(token);
+    try {
+        const decoded = jwtDecode(token);
 
-    return {
-      userId: decoded.userId,
-      userName: decoded.name,
-      // userImage: decoded.userImage || "../../images/anonymous.svg", // 기본 이미지 설정
-    };
-  } catch (error) {
-    console.error("Error decoding JWT:", error);
-    return null;
-  }
+        return {
+            userId: decoded.userId,
+            userName: decoded.name,
+            userImage: decoded.image || userImage,
+        };
+    } catch (error) {
+        console.error("Error decoding JWT:", error);
+        return null;
+    }
 };
